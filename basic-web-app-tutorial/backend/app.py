078bbf4b9350a36443e04a1eff09248623f362fc
Jason <jason5donoghue@gmail.com>
2023-09-30 13:11:11 +0100

    post request working

--- basic-web-app-tutorial/backend/app.py
+++ basic-web-app-tutorial/backend/app.py
@@ -12,14 +12,23 @@ def hello():
     return "Hello, World!"
 
 
-@app.route("/users", methods=["GET"])
+@app.route("/users", methods=["GET", "POST"])
 def users():
     print("users endpoint reached...")
-    with open("users.json", "r") as f:
-        data = json.load(f)
-        data.append({"username": "user4", "pets": ["hamster"]})
-        return flask.jsonify(data)
-
+    if request.method == "GET":
+        with open("users.json", "r") as f:
+            data = json.load(f)
+            data.append({"username": "user4", "pets": ["hamster"]})
+            return flask.jsonify(data)
+    if request.method == "POST":
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        return_data = {
+            "status": "success",
+            "message": f"received: {message}"
+        }
+        return flask.Response(response=json.dumps(return_data), status=201)
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
